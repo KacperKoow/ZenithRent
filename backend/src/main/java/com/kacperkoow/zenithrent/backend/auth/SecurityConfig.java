fc47c340ea5a44d874312205383090fb1b349b7a
KacperKoow <kacper.kowalski799@gmail.com>
2026-07-17 11:44:09 +0200

    feat: configure endpoint security rules and implement role hierarchy

--- backend/src/main/java/com/kacperkoow/zenithrent/backend/auth/SecurityConfig.java
+++ backend/src/main/java/com/kacperkoow/zenithrent/backend/auth/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,18 +23,31 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
+    public RoleHierarchy roleHierarchy() {
+
+        return RoleHierarchyImpl.withDefaultRolePrefix()
+                .role("ADMIN").implies("EMPLOYEE")
+                .role("EMPLOYEE").implies("CUSTOMER")
+                .build();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-
                         .requestMatchers(HttpMethod.GET, "/api/cars/**").permitAll()
 
-                        .requestMatchers(HttpMethod.GET, "/api/bookings").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/cars/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.PUT, "/api/cars/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.DELETE, "/api/cars/**").hasRole("EMPLOYEE")
 
-                        .requestMatchers(HttpMethod.POST, "/api/bookings").hasAnyRole("CUSTOMER", "EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/bookings/my").hasAnyRole("CUSTOMER", "EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/bookings/**").hasRole("CUSTOMER")
+                        .requestMatchers(HttpMethod.GET, "/api/bookings/my").hasRole("CUSTOMER")
+
+
+                        .requestMatchers(HttpMethod.GET, "/api/bookings/**").hasRole("EMPLOYEE")
 
                         .anyRequest().authenticated()
                 )
